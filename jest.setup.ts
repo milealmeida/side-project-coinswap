65392ec89db50cf09a35d63678b65d37ea5f5a41
Marcos Mendes <marcosdev.me@gmail.com>
2023-06-26 20:15:42 -0300

    chore: configure resizeObserver

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,9 @@
 // jest.setup.ts
 import '@testing-library/jest-dom';
+import 'jest-fetch-mock';
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+  disconnect: jest.fn()
+}));
