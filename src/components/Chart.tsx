5913ed79eccea6619e251639605838c3525b1bc5
Marcos Mendes <marcosdev.me@gmail.com>
2023-06-28 18:27:07 -0300

    Update Chart.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -13,26 +13,26 @@ export type ChartProps = {
   data: [];
 };
 
-export default function Chart({ data }: ChartProps) {
-  return (
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart
-        data={data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="name" fill="#8884d8" />
-        <Bar dataKey="priceUsd" fill="#82ca9d" />
-      </BarChart>
-    </ResponsiveContainer>
-  );
-}
+const Chart = ({ data }: ChartProps) => (
+  <ResponsiveContainer width="100%" height={400}>
+    <BarChart
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey="name" fill="#8884d8" />
+      <Bar dataKey="priceUsd" fill="#82ca9d" />
+    </BarChart>
+  </ResponsiveContainer>
+);
+
+export default Chart;
